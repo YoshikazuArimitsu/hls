b542899a257c9cdd1f3693863a3b20c91d60cb33
Y.Arimitsu <yarimit@gmail.com>
2019-12-12 01:05:30 +0900

    Updates

---
+++ precache-manifest.43040be0cbea288f5b808d8f2136750a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fc81f8952d8379dbd7dbe358e7706d",
+    "url": "/hls/index.html"
+  },
+  {
+    "revision": "335cacb6f16cbf41371c",
+    "url": "/hls/static/css/2.e3795789.chunk.css"
+  },
+  {
+    "revision": "331c2b7860566e986517",
+    "url": "/hls/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "335cacb6f16cbf41371c",
+    "url": "/hls/static/js/2.d580c55d.chunk.js"
+  },
+  {
+    "revision": "527b39bc246ae2252e39c047b5f6c865",
+    "url": "/hls/static/js/2.d580c55d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "331c2b7860566e986517",
+    "url": "/hls/static/js/main.c768ef81.chunk.js"
+  },
+  {
+    "revision": "e0d0550d446bfc216185",
+    "url": "/hls/static/js/runtime-main.04bb4697.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hls/static/media/logo.5d5d9eef.svg"
+  }
+]);
